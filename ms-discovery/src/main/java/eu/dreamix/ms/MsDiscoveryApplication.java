e00b759ec35424fbce4cec9f47fd5a62c5ca745c
isilona <iivanov+github.com@isilona.com>
2017-10-06 09:18:22 +0300

    first commit

---
+++ ms-discovery/src/main/java/eu/dreamix/ms/MsDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package eu.dreamix.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MsDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDiscoveryApplication.class, args);
+	}
+}
