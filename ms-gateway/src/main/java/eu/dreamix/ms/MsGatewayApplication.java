f3f95aeb6e76375a68a63b8d978ec86a7d838aa4
isilona <iivanov+github.com@isilona.com>
2017-10-06 22:22:17 +0300

    remove corsFilter

--- ms-gateway/src/main/java/eu/dreamix/ms/MsGatewayApplication.java
+++ ms-gateway/src/main/java/eu/dreamix/ms/MsGatewayApplication.java
@@ -18,21 +18,4 @@ public class MsGatewayApplication {
         SpringApplication.run(MsGatewayApplication.class, args);
     }
 
-    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowCredentials(true);
-        corsConfig.addAllowedOrigin("*");
-        corsConfig.addAllowedHeader("*");
-        corsConfig.addAllowedMethod("OPTIONS");
-        corsConfig.addAllowedMethod("HEAD");
-        corsConfig.addAllowedMethod("GET");
-        corsConfig.addAllowedMethod("PUT");
-        corsConfig.addAllowedMethod("POST");
-        corsConfig.addAllowedMethod("DELETE");
-        corsConfig.addAllowedMethod("PATCH");
-        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfig);
-        return new CorsFilter(urlBasedCorsConfigurationSource);
-    }
 }
