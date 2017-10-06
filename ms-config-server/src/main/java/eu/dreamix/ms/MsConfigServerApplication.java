b5d8065b894794fed38bd8596e2d860a02673f63
isilona <iivanov+github.com@isilona.com>
2017-10-06 14:44:11 +0300

    add config server as eureka client

--- ms-config-server/src/main/java/eu/dreamix/ms/MsConfigServerApplication.java
+++ ms-config-server/src/main/java/eu/dreamix/ms/MsConfigServerApplication.java
@@ -2,15 +2,15 @@ package eu.dreamix.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 public class MsConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsConfigServerApplication.class, args);
+    }
 }
