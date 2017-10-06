50086b7c610ee8586f017a60385e1f86062453a3
isilona <iivanov+github.com@isilona.com>
2017-10-06 09:26:26 +0300

    init discovery server

--- ms-discovery/src/main/java/eu/dreamix/ms/MsDiscoveryApplication.java
+++ ms-discovery/src/main/java/eu/dreamix/ms/MsDiscoveryApplication.java
@@ -2,8 +2,9 @@ package eu.dreamix.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+@EnableEurekaServer
 @SpringBootApplication
 public class MsDiscoveryApplication {
 
