a6a4cb8cbec7819ac8bec8efe5751acf1fdcd0d7
iivanov <iivanov+github.com@isilona.com>
2017-10-29 07:49:43 +0200

    remove hardcoded string and read it from configuration

--- ms-user/src/main/java/eu/dreamix/ms/service/UserServiceImpl.java
+++ ms-user/src/main/java/eu/dreamix/ms/service/UserServiceImpl.java
@@ -4,12 +4,15 @@ import eu.dreamix.ms.entity.User;
 import eu.dreamix.ms.kafka.producer.Sender;
 import eu.dreamix.ms.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserServiceImpl implements UserService {
 
-    private static String HELLOWORLD_TOPIC = "USER_CREATED_TOPIC";
+    @Value("${spring.kafka.topic.userCreated}")
+    private static String USER_CREATED_TOPIC;
+
     private UserRepository userRepository;
     private Sender sender;
 
@@ -21,9 +24,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User registerUser(User input) {
-
         User createdUser = userRepository.save(input);
-        sender.send(HELLOWORLD_TOPIC, createdUser);
+        sender.send(USER_CREATED_TOPIC, createdUser);
         return createdUser;
     }
 
