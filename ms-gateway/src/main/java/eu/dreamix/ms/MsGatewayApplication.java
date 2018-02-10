0b7907e6d3d661b6da94ce17ed56e5865e8d18fc
iivanov <iivanov+github.com@isilona.com>
2018-02-10 17:47:19 +0200

    fix imports and whitespaces

--- ms-gateway/src/main/java/eu/dreamix/ms/MsGatewayApplication.java
+++ ms-gateway/src/main/java/eu/dreamix/ms/MsGatewayApplication.java
@@ -4,10 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @EnableEurekaClient
 @EnableZuulProxy
--- ms-user/src/main/java/eu/dreamix/ms/service/UserServiceImpl.java
+++ ms-user/src/main/java/eu/dreamix/ms/service/UserServiceImpl.java
@@ -5,7 +5,6 @@ import eu.dreamix.ms.kafka.producer.Sender;
 import eu.dreamix.ms.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
